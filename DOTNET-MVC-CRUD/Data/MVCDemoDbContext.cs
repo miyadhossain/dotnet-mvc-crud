ace9629e6dc9dd5cea448ffa492b5bb4e56505af
miyadhossain <miyad.mh@gmail.com>
2023-05-17 23:07:38 +0600

    dotnet mvc crud done

---
+++ DOTNET-MVC-CRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DOTNET_MVC_CRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DOTNET_MVC_CRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
